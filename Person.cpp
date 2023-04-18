d38936ded570e300577d374f25d776167b6d2230
Oliver Nikolajevic <130287490+OliverNikolajevic@users.noreply.github.com>
2023-04-18 18:56:28 +0200

    Update Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -1,28 +1,55 @@
+/*This program defines two classes, Person and Database, which are used to store and manipulate 
+information about people and their calorie needs.
+
+The Person class has private member variables for a person's name, last name, weight, 
+and height. It also has a constructor that can initialize these variables, as well as a
+default constructor and a setDetails() method that prompts the user to enter information 
+about a person's name, last name, weight, and height.
+
+The Person class also has a calculateCalories() method that takes an activity level as 
+a parameter and calculates the number of calories needed per day to lose weight, based 
+on the person's weight, height, and basal metabolic rate (BMR). It uses the Harris-Benedict
+equation to calculate the BMR, which takes into account a person's weight, height, age, and gender.
+The displayCalories() method takes an activity level as a parameter and displays the number of 
+calories needed to lose weight for a particular person.
+
+The Database class uses a std::map to store instances of the Person class, with the key 
+being the person's name and last name. It has methods to add a person, remove a person,
+update a person's details, display all people, and display calories for a specific person.
+
+The main() function presents a menu of options for the user to choose from, and calls the
+appropriate methods of the Database class based on the user's input.
+
+Overall, this program provides a simple way to store and manage information about 
+people's calorie needs, and could be useful for individuals or professionals in the 
+health and fitness industry. However, it could benefit from additional error checking 
+and input validation to improve its robustness and reliability.
+*/
 #include <iostream>
 #include <string>
 #include <map>
 using namespace std;
 class Person {
 private:
-    std::string name;
-    std::string lastName;
+    string name;
+    string lastName;
     double weight;
     double height;
 
 public:
     Person() {}
 
-    Person(std::string n, std::string ln, double w, double h) : name(n), lastName(ln), weight(w), height(h) {}
+    Person(string n,string ln, double w, double h) : name(n), lastName(ln), weight(w), height(h) {}
 
     void setDetails() {
-        std::cout << "Enter name: ";
-        std::getline(std::cin, name);
-        std::cout << "Enter last name: ";
-        std::getline(std::cin, lastName);
-        std::cout << "Enter weight (in kg): ";
-        std::cin >> weight;
-        std::cout << "Enter height (in cm): ";
-        std::cin >> height;
+        cout << "Enter name: ";
+        getline(cin, name);
+        cout << "Enter last name: ";
+        getline(cin, lastName);
+        cout << "Enter weight (in kg): ";
+        cin >> weight;
+        cout << "Enter height (in cm): ";
+        cin >> height;
     }
 
     double calculateCalories(double activityLevel) {
@@ -35,14 +62,14 @@ public:
 
     void displayCalories(double activityLevel) {
         double caloriesToLose = calculateCalories(activityLevel);
-        std::cout << "Calories needed to lose weight: " << caloriesToLose << " kcal/day" << std::endl;
+        cout << "Calories needed to lose weight: " << caloriesToLose << " kcal/day" << endl;
     }
 
-    std::string getName() {
+    string getName() {
         return name;
     }
 
-    std::string getLastName() {
+    string getLastName() {
         return lastName;
     }
 
@@ -57,29 +84,29 @@ public:
 
 class Database {
 private:
-    std::map<std::string, Person> db;
+    map<string, Person> db;
 
 public:
     void addPerson(Person p) {
-        db.insert(std::make_pair(p.getName() + " " + p.getLastName(), p));
+        db.insert(make_pair(p.getName() + " " + p.getLastName(), p));
     }
 
-    void removePerson(std::string name, std::string lastName) {
-        std::string fullName = name + " " + lastName;
+    void removePerson(string name,string lastName) {
+        string fullName = name + " " + lastName;
         db.erase(fullName);
     }
 /*
-    void updatePerson(std::string name, std::string lastName, Person p) {
-        std::string fullName = name + " " + lastName;
+    void updatePerson(string name, string lastName, Person p) {
+        string fullName = name + " " + lastName;
         db[fullName] = p;
     }
     */
     void updatePerson() {
-        std::string name, lastName;
-        std::cout << "Enter name: ";
-        std::getline(std::cin, name);
-        std::cout << "Enter last name: ";
-        std::getline(std::cin, lastName);
+        string name, lastName;
+        cout << "Enter name: ";
+        getline(cin, name);
+        cout << "Enter last name: ";
+        getline(cin, lastName);
         auto it = db.find(name + " " + lastName);
         if (it != db.end()) {
             Person p = it->second;
@@ -87,37 +114,37 @@ public:
             db[name + " " + lastName] = p;
         }
         else {
-            std::cout << "Person not found" << std::endl;
+            cout << "Person not found" << endl;
         }
     }
 
     void displayAll() {
         for (auto it = db.begin(); it != db.end(); it++) {
-            std::cout << "Name: " << it->second.getName() << std::endl;
-            std::cout << "Last Name: " << it->second.getLastName() << std::endl;
-            std::cout << "Weight: " << it->second.getWeight() << " kg" << std::endl;
-            std::cout << "Height: " << it->second.getHeight() << " cm" << std::endl;
-            std::cout << std::endl;
+           cout << "Name: " << it->second.getName() << endl;
+           cout << "Last Name: " << it->second.getLastName() << endl;
+           cout << "Weight: " << it->second.getWeight() << " kg" << endl;
+           cout << "Height: " << it->second.getHeight() << " cm" << endl;
+           cout << endl;
         }
     }
 
-    void displayCalories(std::string name, std::string lastName, double activityLevel) {
-        std::string fullName = name + " " + lastName;
+    void displayCalories( string name, string lastName, double activityLevel) {
+         string fullName = name + " " + lastName;
         auto it = db.find(fullName);
         if (it == db.end()) {
-            cout << "Person not found" << std::endl;
+            cout << "Person not found" <<  endl;
         }
         else {
-            std::cout << "Name: " << it->second.getName() << std::endl;
-            std::cout << "Last Name: " << it->second.getLastName() << std::endl;
-            std::cout << "Weight: " << it->second.getWeight() << " kg" << std::endl;
-            std::cout << "Height: " << it->second.getHeight() << " cm" << std::endl;
-            std::cout << std::endl;
+             cout << "Name: " << it->second.getName() <<  endl;
+             cout << "Last Name: " << it->second.getLastName() <<  endl;
+             cout << "Weight: " << it->second.getWeight() << " kg" <<  endl;
+             cout << "Height: " << it->second.getHeight() << " cm" <<  endl;
+             cout <<  endl;
             it->second.displayCalories(activityLevel);
         }
     }
-    Person* find(std::string name, std::string lastName) {
-        std::string fullName = name + " " + lastName;
+    Person* find( string name,  string lastName) {
+         string fullName = name + " " + lastName;
         auto it = db.find(fullName);
         if (it == db.end()) {
             return nullptr;
@@ -134,16 +161,16 @@ int main() {
     Database db;
     int choice;
     do {
-        std::cout << "1. Add person" << std::endl;
-        std::cout << "2. Remove person" << std::endl;
-        std::cout << "3. Update person" << std::endl;
-        std::cout << "4. Display all persons" << std::endl;
-        std::cout << "5. Display calories for person" << std::endl;
-        std::cout << "6. Exit" << std::endl;
-        std::cout << "Enter your choice: ";
-        std::cin >> choice;
-        std::cin.ignore(); // ignore the newline character
-        std::cout << std::endl;
+         cout << "1. Add person" <<  endl;
+         cout << "2. Remove person" <<  endl;
+         cout << "3. Update person" <<  endl;
+         cout << "4. Display all persons" <<  endl;
+         cout << "5. Display calories for person" <<  endl;
+         cout << "6. Exit" <<  endl;
+         cout << "Enter your choice: ";
+         cin >> choice;
+         cin.ignore(); // ignore the newline character
+         cout <<  endl;
         switch (choice) {
         case 1: {
             Person p;
@@ -153,11 +180,11 @@ int main() {
             break;
         }
         case 2: {
-            std::string name, lastName;
-            std::cout << "Enter name: ";
-            std::getline(std::cin, name);
-            std::cout << "Enter last name: ";
-            std::getline(std::cin, lastName);
+             string name, lastName;
+             cout << "Enter name: ";
+             getline( cin, name);
+             cout << "Enter last name: ";
+             getline( cin, lastName);
             db.removePerson(name, lastName);
             break;
         }
@@ -173,27 +200,27 @@ int main() {
             break;
         }
         case 5: {
-            std::string name, lastName;
-            std::cout << "Enter name: ";
-            std::getline(std::cin, name);
-            std::cout << "Enter last name: ";
-            std::getline(std::cin, lastName);
+             string name, lastName;
+             cout << "Enter name: ";
+             getline( cin, name);
+             cout << "Enter last name: ";
+             getline( cin, lastName);
             double activityLevel;
-            std::cout << "Enter activity level: ";
-            std::cin >> activityLevel;
+             cout << "Enter activity level: ";
+             cin >> activityLevel;
             db.displayCalories(name, lastName, activityLevel);
             break;
         }
         case 6: {
-            std::cout << "Exiting program" << std::endl;
+             cout << "Exiting program" <<  endl;
             break;
         }
         default: {
-            std::cout << "Invalid choice" << std::endl;
+             cout << "Invalid choice" <<  endl;
             break;
         }
         }
-        std::cout << std::endl;
+         cout <<  endl;
     } while (choice != 6);
     //The game was developed by Oliver Nikolajevic
     return 0;
